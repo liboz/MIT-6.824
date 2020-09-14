1e032534d007dc34c31a5a1c0cf75bd9b1e50c47
Libo Zeng <ringil.sword@gmail.com>
2020-09-13 23:35:27 -0400

    update printing

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,9 +1,13 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"os"
+	"strconv"
+)
 
 // Debugging
-const Debug = 0
+var Debug = parseEnvOrDefault("RAFT_DEBUG", 0)
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
@@ -31,4 +35,14 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func parseEnvOrDefault(envVarName string, defaultValue int) int {
+	raftDebug := os.Getenv(envVarName)
+	i, err := strconv.Atoi(raftDebug)
+	if err != nil {
+		return defaultValue
+	} else {
+		return i
+	}
 }
