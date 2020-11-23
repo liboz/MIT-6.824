1b19185372f114c5845e307013930781fdb85a4e
Libo Zeng <ringil.sword@gmail.com>
2020-11-23 09:04:03 -0500

    optimize shardmaster

--- src/shardmaster/client.go
+++ src/shardmaster/client.go
@@ -15,8 +15,9 @@ import (
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// Your data here.
-	id              int64
-	operationNumber int
+	id               int64
+	operationNumber  int
+	lastLeaderServer int
 }
 
 func nrand() int64 {
@@ -32,7 +33,18 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	// Your code here.
 	ck.id = nrand()
 	ck.operationNumber = 0
+	ck.lastLeaderServer = -1
 	return ck
+}
+
+func (ck *Clerk) getInitialServer() int {
+	var initialServer int
+	if ck.lastLeaderServer == -1 {
+		initialServer = 0
+	} else {
+		initialServer = ck.lastLeaderServer
+	}
+	return initialServer
 }
 
 func (ck *Clerk) Query(num int) Config {
@@ -43,9 +55,11 @@ func (ck *Clerk) Query(num int) Config {
 	args.ClientInfo.ClientOperationNumber = ck.operationNumber
 	args.Num = num
 	responseCh := make(chan *QueryReply)
+	initialServer := ck.getInitialServer()
 	for {
 		// try each known server.
-		for _, srv := range ck.servers {
+		for i := initialServer; i < initialServer+len(ck.servers); i++ {
+			srv := ck.servers[i%len(ck.servers)]
 			go func(srv *labrpc.ClientEnd) {
 				reply := &QueryReply{}
 				ok := srv.Call("ShardMaster.Query", args, &reply)
@@ -53,16 +67,18 @@ func (ck *Clerk) Query(num int) Config {
 					responseCh <- reply
 				}
 			}(srv)
-		}
-		select {
-		case <-time.After(time.Duration(500 * time.Millisecond)):
-			continue
-		case reply := <-responseCh:
-			if !reply.WrongLeader {
-				return reply.Config
+			select {
+			case <-time.After(time.Duration(500 * time.Millisecond)):
+				continue
+			case reply := <-responseCh:
+				if !reply.WrongLeader {
+					ck.lastLeaderServer = i % len(ck.servers)
+					return reply.Config
+				}
 			}
+			time.Sleep(100 * time.Millisecond)
 		}
-		time.Sleep(100 * time.Millisecond)
+
 	}
 }
 
@@ -73,10 +89,13 @@ func (ck *Clerk) QueryHigher(num int) []Config {
 	args.ClientInfo.ClientId = ck.id
 	args.ClientInfo.ClientOperationNumber = ck.operationNumber
 	args.Num = num
+	initialServer := ck.getInitialServer()
+
 	responseCh := make(chan *QueryHigherReply)
 	for {
-		// try each known server.
-		for _, srv := range ck.servers {
+		for i := initialServer; i < initialServer+len(ck.servers); i++ {
+			srv := ck.servers[i%len(ck.servers)]
+			// try each known server.
 			go func(srv *labrpc.ClientEnd) {
 				reply := &QueryHigherReply{}
 				ok := srv.Call("ShardMaster.QueryHigher", args, &reply)
@@ -84,16 +103,17 @@ func (ck *Clerk) QueryHigher(num int) []Config {
 					responseCh <- reply
 				}
 			}(srv)
-		}
-		select {
-		case <-time.After(time.Duration(500 * time.Millisecond)):
-			continue
-		case reply := <-responseCh:
-			if !reply.WrongLeader {
-				return reply.Configs
+			select {
+			case <-time.After(time.Duration(500 * time.Millisecond)):
+				continue
+			case reply := <-responseCh:
+				if !reply.WrongLeader {
+					ck.lastLeaderServer = i % len(ck.servers)
+					return reply.Configs
+				}
 			}
+			time.Sleep(100 * time.Millisecond)
 		}
-		time.Sleep(100 * time.Millisecond)
 	}
 }
 
